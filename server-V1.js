8452ff0eafcafa8ca5d331180a4f9e37eb938b00
TomDadongHu <69221143+TomDadongHu@users.noreply.github.com>
2022-06-14 06:29:32 -0700

    server new

--- server-V1.js
+++ server-V1.js
@@ -7,9 +7,9 @@ const cors = require('cors')
 const mysql = require("mysql");
 
 const db = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"password",
+    host:"free-db.ccj1y6o1kv7w.us-east-2.rds.amazonaws.com",
+    user:"admin",
+    password:"hudadong123",
     database:"webxr_data"
 })
 
