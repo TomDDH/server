8a978e8fa2c39bd3fd42aa174e22fc6a65052be1
TomDDH <69221143+TomDDH@users.noreply.github.com>
2022-06-16 23:47:44 -0700

    Add files via upload

--- mysql/webxr_data_product_data.sql
+++ mysql/webxr_data_product_data.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
--- Host: localhost    Database: webxr_data
+-- Host: free-db.ccj1y6o1kv7w.us-east-2.rds.amazonaws.com    Database: webxr_data
 -- ------------------------------------------------------
 -- Server version	8.0.29
 
@@ -14,6 +14,14 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
 
 --
 -- Table structure for table `product_data`
@@ -23,13 +31,14 @@ DROP TABLE IF EXISTS `product_data`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product_data` (
-  `data_id` int NOT NULL,
-  `product_name` text,
-  `info_1` text,
-  `info_2` text,
-  `info_3` text,
-  PRIMARY KEY (`data_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(40) DEFAULT NULL,
+  `info_1` varchar(255) DEFAULT NULL,
+  `info_2` varchar(255) DEFAULT NULL,
+  `info_3` varchar(255) DEFAULT NULL,
+  `ar_model` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,9 +47,10 @@ CREATE TABLE `product_data` (
 
 LOCK TABLES `product_data` WRITE;
 /*!40000 ALTER TABLE `product_data` DISABLE KEYS */;
-INSERT INTO `product_data` VALUES (1,'product 1','info about this proct 1','more info about this proct 1',' still more info about this proct 1'),(2,'product 2','info about this proct 2','more info about this proct 2',' still more info about this proct 2'),(3,'product 3','info about this proct 3','more info about this proct 3',' still more info about this proct 3'),(4,'product 4','info about this proct 4','more info about this proct 4',' still more info about this proct 4'),(5,'product 5','info about this proct 5','more info about this proct 5',' still more info about this proct 5');
+INSERT INTO `product_data` VALUES (1,'product bed','info about this proct bed','more info about this proct bed',' still more info about this proct bed','bed.glb'),(2,'product chair','info about this proct chair','more info about this proct chair',' still more info about this proct chair','chair.glb'),(3,'product global','info about this proct global','more info about this proct global',' still more info about this proct global','global.glb'),(4,'product sofa','info about this proct sofa','more info about this proct sofa',' still more info about this proct sofa','sofa.glb'),(5,'product table','info about this proct table','more info about this proct table',' still more info about this proct table','table.glb');
 /*!40000 ALTER TABLE `product_data` ENABLE KEYS */;
 UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -51,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-16 23:30:26
+-- Dump completed on 2022-06-16 23:47:27
