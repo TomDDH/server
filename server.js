dba5e577b76418a4128dfa7209459f2df4975579
TomDadongHu <69221143+TomDadongHu@users.noreply.github.com>
2022-06-16 23:54:01 -0700

    change

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const mysql = require("mysql");
 const db = mysql.createPool({
   host:"localhost",
   user:"root",
-  password:"Dadonghu123s!",
+  password:"Dadonghu123!",
   database:"webxr_data"
 })
 
